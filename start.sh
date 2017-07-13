eb4a6d7fd4ed37502b1ebfecf95b0a697d4a6313
Kevin Peterson <kevin.peterson@mayo.edu>
2017-07-13 08:42:36 -0500

    Allow for a random admin password.

--- start.sh
+++ start.sh
@@ -1,9 +1,24 @@
-SERVER_HOST=$1
+#!/usr/bin/env bash
 
-if [ -z "$1" ]
+while getopts ":h:r" opt; do
+  case $opt in
+    h)
+      SERVER_HOST=$OPTARG
+      ;;
+    r)
+      ADMIN_PASSWORD="$(LC_CTYPE=C tr -dc A-Za-z0-9_\!\@\#\$\%\^\&\*\(\)-+= < /dev/urandom | head -c 32 | xargs)"
+      ;;
+    \?)
+      echo "Invalid option: -$OPTARG" >&2
+      exit 1
+      ;;
+  esac
+done
+
+if [ -z "$SERVER_HOST" ]
   then
     echo "No external facing host name was passed in."
-    echo " - example: start.sh myhost.org"
+    echo " - example: start.sh -h myhost.org"
     echo ""
     echo "Attempting to guess target host name."
     case "$(uname -s)" in
@@ -36,6 +51,9 @@ if [ ! -f ./ssl/cert.haproxy ]; then
     cd ..
 fi
 
+if [ $ADMIN_PASSWORD ]; then
+    export ADMIN_PASSWORD=$ADMIN_PASSWORD
+fi
 export SERVER_HOST=$SERVER_HOST
 export EXTERNAL_URL=https://$SERVER_HOST
 export DEFAULT_SSL_CERT="$(cat ./ssl/cert.haproxy)"
@@ -47,3 +65,7 @@ docker-compose up -d
 echo ""
 echo "Value Set Workbench is starting at https://$SERVER_HOST/"
 echo " * NOTE: Service may take a few minutes to start."
+
+if [ $ADMIN_PASSWORD ]; then
+    echo "Admin username/password: admin/$ADMIN_PASSWORD"
+fi
